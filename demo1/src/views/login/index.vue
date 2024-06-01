acf35ca273c6b352e18db4925060c469523c03eb
wenxiang6 <878265095@qq.com>
2024-06-01 11:45:54 +0800

    项目初始化

---
+++ demo1/src/views/login/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="login">
+    <LoginOn/>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.login {
+  width: 100%;
+  height: 100%;
+  background-image: url('@/pic/loginBackground.png');
+  background-size: cover;
+  /* 背景图像缩放以完全覆盖元素 */
+  background-position: center;
+  /* 背景图像居中 */
+  padding: 25px;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: right;
+  align-items: center;
+}
+</style>
